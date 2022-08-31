7b81835448ea39144e26501b6d24e5a1caf39710
Tuğçe Pala <64085374+TugcePala@users.noreply.github.com>
2022-08-31 08:40:56 +0300

    Update baglanti.php

--- tez/baglanti.php
+++ tez/baglanti.php
@@ -1,6 +1,6 @@
 <?php
   try{
-      $db=new PDO("mysql:host=localhost;dbname=tezornek;charset=utf8",'root','12345678');
+      $db=new PDO("mysql:host=localhost;dbname=tezornek;charset=utf8",'root','');
      // echo "Bağlantı başarılı";
   }
  catch(PDOExeption $e){
